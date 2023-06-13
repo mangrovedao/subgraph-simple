91268bb22e471b38dda17306a808b7f1b5f1b951
Louis-Amas <amaslouis@protonmail.com>
2023-06-13 14:23:42 +0200

    feat: add MangroveOrder support

---
+++ src/mangrove-order.ts
@@ -0,0 +1,37 @@
+import {
+  LogIncident,
+  Mgv,
+  NewOwnedOffer,
+  OrderSummary,
+  SetAdmin,
+  SetExpiry,
+  SetRouter
+} from "../generated/MangroveOrder/MangroveOrder"
+import { Offer } from "../generated/schema"
+import { getOfferId } from "./helpers"
+
+export function handleLogIncident(event: LogIncident): void {}
+
+export function handleMgv(event: Mgv): void {}
+
+export function handleNewOwnedOffer(event: NewOwnedOffer): void {
+  const offerId = getOfferId(
+    event.params.outbound_tkn, 
+    event.params.inbound_tkn, 
+    event.params.offerId
+  );
+  const offer = Offer.load(offerId)!;
+
+  offer.owner = event.params.owner;
+
+  offer.save();
+
+}
+
+export function handleOrderSummary(event: OrderSummary): void {}
+
+export function handleSetAdmin(event: SetAdmin): void {}
+
+export function handleSetExpiry(event: SetExpiry): void {}
+
+export function handleSetRouter(event: SetRouter): void {}
