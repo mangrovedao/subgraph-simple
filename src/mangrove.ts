c073902c3b85e4b19d7aca03e0336d5aa3d31545
Louis-Amas <amaslouis@protonmail.com>
2023-06-20 10:17:49 +0200

    feat: add outbound_tkn and inbound_tkn to order

--- src/mangrove.ts
+++ src/mangrove.ts
@@ -193,6 +193,8 @@ export function handleOrderComplete(event: OrderComplete): void {
   order.penalty = event.params.penalty;
   order.feePaid = event.params.feePaid;
 
+  const market = Market.load(getMarketId(event.params.outbound_tkn, event.params.inbound_tkn))!;
+  order.market = market.id;
   order.save();
 
   removeOrderFromStack();
